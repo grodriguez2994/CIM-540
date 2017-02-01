ddcb261a7ad1675b31cfb59e575b97729cf1eb50
grodriguez2994 <grodriguez2994>
2017-02-01 15:32:03 -0500

    Week 2 assignmnet

--- Week 2/Week 2 Assignment/sketch.js
+++ Week 2/Week 2 Assignment/sketch.js
@@ -1,7 +1,32 @@
-function setup() {
+var mouthX = 100; 
+var mouthY = 140; 
+var mouthW = 400; 
+var mouthH = 500; 
 
+
+function setup() {    
+createCanvas(500,400); 
+background(100,20,200); 
 }
 
 function draw() {
-  
+fill(255,255,255); 
+strokeWeight(2.5);
+
+ellipse(mouthX, mouthY, 100, 100); 
+ellipse(mouthX + 300, mouthY,100,100); 
+    
+//ellipse (100,140,100,100);
+//ellipse (400,140,100,100); 
+    
+    
+arc(250, 400, 280, 280, PI, TWO_PI); 
+
+fill(0,0,0); 
+//ellipse (80,150,40,40); 
+//ellipse (380,150,40,40); 
+ellipse(mouthX-20,mouthY+10,40,40);
+ellipse(mouthX+280,mouthY+10,40,40);  
+    
 }
+
--- Week 2/Week 2 Class Notes/sketch.js
+++ Week 2/Week 2 Class Notes/sketch.js
@@ -1,7 +1,71 @@
-function setup() {
+//var num = 100; 
+//var num2 = 50; 
+//var message = "hello"; 
+//var trex = 1000; 
+//var anotherMessage = "goodbye";
+//var gabriela = 7; 
+//var booleanVar = true;
+//var booleanVar2 = false; 
 
+var mouthX = 50; 
+var mouthY = 300; 
+var mouthW = 400; 
+var mouthH = 40; 
+
+
+function setup() {
+//console.log(num); 
+//console.log("num + num2 = " + (num + num2)); 
+//console.log (num * num2); 
+//console.log(num/num2); 
+//console.log(message + anotherMessage); 
+//console.log(message + " " + anotherMessage); 
+//console.log(gabriela); 
+//console.log(booleanVar); 
+//console.log (message + trex);
+//    
+////This is not going to work 
+//console.log(message * trex); 
+//    
+///* If you want
+//to write more stuff
+//use a multi-line comment */ 
+//    
+//var message2 = "This only works in the setup"; 
+//console.log(message2); 
+//console.log(message3); 
+//    
+
+createCanvas(500,400); 
+background(240,230,240); 
+
+console.log(width + " " + height)
 }
+
 
 function draw() {
-  
+//    console.log(num);  
+//    console.log (message2); 
+//    var message3 = "This only works in the draw"; 
+    
+    //z,y,width,height 
+ellipse(70,70,100,100); 
+    
+ellipse(width/2,height/2,50,50); 
+    
+rect(mouthX, mouthY, mouthW, mouthH);  
+line(mouthX, mouthY + mouthH/2, mouthX + mouthW, mouthY + mouthH/2); 
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
